53da7d0849de9160ce169bb409b8b55d6657d349
Omkar Yadav <55269334+omkar-yadav-12@users.noreply.github.com>
2020-07-14 17:04:09 -0500

    Bug fix

--- app/json.js
+++ app/json.js
@@ -4,20 +4,20 @@ const api = require('./controllers/api')
 async function updateAllEvents() {
     let events = await api.call('event?region_key=IA&season_key=1920')
     console.log(events)
-    fs.writeFile("app/apiData/events/allEvents.json", JSON.stringify(events), (err) => {
+    fs.writeFile("app/data/events/allEvents.json", JSON.stringify(events), (err) => {
         if (err) console.error(err)
     })
 }
 
 async function updateEvents(link, path) {
-    console.log("app/apiData/events/" + path + ".json")
-    let allEvents = fs.readFileSync('app/apiData/events/allEvents.json')
+    console.log("app/data/events/" + path + ".json")
+    let allEvents = fs.readFileSync('app/data/events/allEvents.json')
     let data = JSON.parse(allEvents)
     let array = []
     for (let i = 0; i < data.length; i++) {
         array.push(await api.call('event/' + data[i].event_key + link))
     }
-    fs.writeFile("app/apiData/events/" + path + ".json", JSON.stringify(array), (err) => {
+    fs.writeFile("app/data/events/" + path + ".json", JSON.stringify(array), (err) => {
         if (err) console.error(err)
         else console.log("COMPLETE")
     })
@@ -25,47 +25,47 @@ async function updateEvents(link, path) {
 
 async function updateAllTeams() {
     let teams = await api.call('team?region_key=IA&last_active=1920')
-    fs.writeFile("app/apiData/teams/allTeams.json", JSON.stringify(teams), (err) => {
+    fs.writeFile("app/data/teams/allTeams.json", JSON.stringify(teams), (err) => {
         if (err) console.error(err)
         else console.log("COMPLETE")
     })
 }
 
 async function updateTeams(query, path) {
-    let allTeams = fs.readFileSync('app/apiData/teams/allTeams.json')
+    let allTeams = fs.readFileSync('app/data/teams/allTeams.json')
     let data = JSON.parse(allTeams)
     let array = []
     for (let i = 0; i < data.length; i++) {
         array.push(await api.call('team/' + data[i].team_key + '/' + query + '/1920'))
     }
-    fs.writeFile("app/apiData/teams/" + path + ".json", JSON.stringify(array), (err) => {
+    fs.writeFile("app/data/teams/" + path + ".json", JSON.stringify(array), (err) => {
         if (err) console.error(err)
         else console.log("COMPLETE")
     })
 }
 
 async function teamResults() {
-    let allTeams = fs.readFileSync('app/apiData/teams/allTeamsResults.json')
+    let allTeams = fs.readFileSync('app/data/teams/allTeamsResults.json')
     let data = JSON.parse(allTeams)
     let array = []
     console.log(data[1][0])
     for (let i = 0; i < data.length; i++) {
         array.push(await api.call('team/' + data[i] + '/results/1920'))
     }
-    fs.writeFile("app/apiData/teams/allTeamsResults.json", JSON.stringify(array), (err) => {
+    fs.writeFile("app/data/teams/allTeamsResults.json", JSON.stringify(array), (err) => {
         if (err) console.error(err)
         else console.log("COMPLETE")
     })
 }
 
 async function updateTeamsMatches() {
-    let allTeams = fs.readFileSync('app/apiData/teams/allTeams.json')
+    let allTeams = fs.readFileSync('app/data/teams/allTeams.json')
     let data = JSON.parse(allTeams)
     let array = []
     for (let i = 0; i < data.length; i++) {
         array.push(await api.call('team/' + data[i].team_key))
     }
-    fs.writeFile("app/apiData/teams/allTeamsMatchDetails.json", JSON.stringify(array), (err) => {
+    fs.writeFile("app/data/teams/allTeamsMatchDetails.json", JSON.stringify(array), (err) => {
         if (err) console.error(err)
         else console.log("COMPLETE")
     })
@@ -85,7 +85,7 @@ async function updateMatches() {
         }
     }
     console.log(iowaMatches.length)
-    fs.writeFile("app/apiData/matches/allMatches.json", JSON.stringify(iowaMatches), (err) => {
+    fs.writeFile("app/data/matches/allMatches.json", JSON.stringify(iowaMatches), (err) => {
         if (err) console.error(err)
         else console.log("COMPLETE")
     })
@@ -94,13 +94,13 @@ async function updateMatches() {
 }
 
 async function updateMatchesDetails (){
-    let allTeams = fs.readFileSync('app/apiData/matches/allMatches.json')
+    let allTeams = fs.readFileSync('app/data/matches/allMatches.json')
     let data = JSON.parse(allTeams) 
     let details = []
     for (let i = 0; i < data.length; i++) {
         details.push(await api.call('match/' + data[i].match_key + '/details'))
     }
-    fs.writeFile("app/apiData/matches/allMatchesDetails.json", JSON.stringify(details), (err) => {
+    fs.writeFile("app/data/matches/allMatchesDetails.json", JSON.stringify(details), (err) => {
         if (err) console.error(err)
         else console.log("COMPLETE")
     })
@@ -123,26 +123,26 @@ async function updateApiData() {
 
 
 // async function updateAllTeamsMatches() {
-//     let allTeams = fs.readFileSync('app/apiData/teams/allTeams.json')
+//     let allTeams = fs.readFileSync('app/data/teams/allTeams.json')
 //     let data = JSON.parse(allTeams)
 //     let array = []
 //     for (let i = 0; i < data.length; i++) {
 //         array.push(await api.call('team/' + data[i].team_key + '/matches/1920'))
 //     }
-//     fs.writeFile("app/apiData/teams/allTeamsMatches.json", JSON.stringify(array), (err) => {
+//     fs.writeFile("app/data/teams/allTeamsMatches.json", JSON.stringify(array), (err) => {
 //         if (err) console.error(err)
 //         else console.log("COMPLETE")
 //     })
 // }
 
 // async function updateAllTeamsEvents() {
-//     let allTeams = fs.readFileSync('app/apiData/teams/allTeams.json')
+//     let allTeams = fs.readFileSync('app/data/teams/allTeams.json')
 //     let data = JSON.parse(allTeams)
 //     let array = []
 //     for (let i = 0; i < data.length; i++) {
 //         array.push(await api.call('team/' + data[i].team_key + '/events/1920'))
 //     }
-//     fs.writeFile("app/apiData/teams/allTeamsEvents.json", JSON.stringify(array), (err) => {
+//     fs.writeFile("app/data/teams/allTeamsEvents.json", JSON.stringify(array), (err) => {
 //         if (err) console.error(err)
 //         else console.log("COMPLETE")
 //     })
